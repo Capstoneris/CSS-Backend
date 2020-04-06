adf093ffc3911135791b64eac8c0e29d3a84b363
Simon Oyen <prgglptt@gmail.com>
2020-04-06 14:00:49 +0200

    Fixed package name in resource config

--- src/main/java/de/hsh/capstoneris/Main.java
+++ src/main/java/de/hsh/capstoneris/Main.java
@@ -23,7 +23,7 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in org.example.rest package
-        final ResourceConfig rc = new ResourceConfig().register(CORSResponseFilter.class).packages("org.example.rest");
+        final ResourceConfig rc = new ResourceConfig().register(CORSResponseFilter.class).packages("de.hsh.capstoneris.rest");
 
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
