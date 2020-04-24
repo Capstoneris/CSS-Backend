0429ae9f95ea82b70dd15ffb51f9b5a36b033a8e
Simon Schwierzeck <simon.schwierzeck@stud.hs-hannover.de>
2020-04-24 17:26:08 +0200

    added getter, setter and constructors #20 #21

--- src/main/java/de/hsh/capstoneris/socket/SocketUser.java
+++ src/main/java/de/hsh/capstoneris/socket/SocketUser.java
@@ -5,8 +5,27 @@ import de.hsh.capstoneris.socket.session.SocketSession;
 import java.util.ArrayList;
 
 public class SocketUser {
-
     public int sessionID;
     public ArrayList<String> currentRooms;
 
+    public SocketUser(int sessionID, ArrayList<String> currentRooms) {
+        this.sessionID = sessionID;
+        this.currentRooms = currentRooms;
+    }
+
+    public int getSessionID() {
+        return sessionID;
+    }
+
+    public void setSessionID(int sessionID) {
+        this.sessionID = sessionID;
+    }
+
+    public ArrayList<String> getCurrentRooms() {
+        return currentRooms;
+    }
+
+    public void setCurrentRooms(ArrayList<String> currentRooms) {
+        this.currentRooms = currentRooms;
+    }
 }
